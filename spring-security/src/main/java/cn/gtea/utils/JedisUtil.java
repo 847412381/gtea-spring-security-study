f2975321d8b7d31f226ef70ddc7acafb1b13ff7b
mizon <8399460+mizon@user.noreply.gitee.com>
2022-06-02 01:21:28 +0800

    加数据库配置，jedis开发

--- spring-security/src/main/java/cn/gtea/utils/JedisUtil.java
+++ spring-security/src/main/java/cn/gtea/utils/JedisUtil.java
@@ -10,14 +10,54 @@ import redis.clients.jedis.Jedis;
  */
 public class JedisUtil {
 
-    private static Jedis jedis = new Jedis("120.78.220.155", 6379);
+    private static final String IP = "120.78.220.155";
+    private static final Integer PORT = 6379;
+    private static final String AUTH_PASS = "JedisqazWSX..";
+
+    private static Jedis jedis = new Jedis(IP, PORT);
 
     private JedisUtil() {
 
     }
 
     public static Jedis create() {
-        jedis.auth("JedisqazWSX..");
+        jedis.auth(AUTH_PASS);
         return jedis;
+    }
+
+    /**
+     * 建造者模式创建
+     * @return
+     */
+    public static JedisBuilder builder() {
+        return new JedisBuilder();
+    }
+
+    private static class JedisBuilder {
+
+        private String ip;
+        private Integer port;
+        private String password;
+
+        public JedisBuilder auth(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public JedisBuilder ip(String ip) {
+            this.ip = ip;
+            return this;
+        }
+
+        public JedisBuilder port(Integer port) {
+            this.port = port;
+            return this;
+        }
+
+        public Jedis build() {
+            Jedis jedis = new Jedis(ip, port);
+            jedis.auth(password);
+            return jedis;
+        }
     }
 }
