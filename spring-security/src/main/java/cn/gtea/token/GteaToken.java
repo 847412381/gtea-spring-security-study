eec8a53f971dedbc5fd2f50bc165d8f59f7b665d
mizon <8399460+mizon@user.noreply.gitee.com>
2022-06-04 03:01:03 +0800

    token继承类开发

--- spring-security/src/main/java/cn/gtea/token/GteaToken.java
+++ spring-security/src/main/java/cn/gtea/token/GteaToken.java
@@ -1,22 +1,41 @@
 package cn.gtea.token;
 
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
+
 import java.util.Collection;
 
 /**
  * @author Ayeze_Mizon
  * 2022-05-12
  */
-public class GteaToken extends UsernamePasswordAuthenticationToken {
+public class GteaToken extends AbstractAuthenticationToken {
 
     private static final long serialVersionUID = -3330109676428092468L;
 
+    private final Object principal;
+    private Object credentials;
+
     public GteaToken(Object principal, Object credentials) {
-        super(principal, credentials);
+        super(null);
+        this.principal = principal;
+        this.credentials = credentials;
+        setAuthenticated(false);
     }
 
     public GteaToken(Object principal, Object credentials, Collection<? extends GrantedAuthority> authorities) {
-        super(principal, credentials, authorities);
+        super(authorities);
+        this.principal = principal;
+        this.credentials = credentials;
+        super.setAuthenticated(true); // must use super, as we override
     }
+
+    public Object getCredentials() {
+        return this.credentials;
+    }
+
+    public Object getPrincipal() {
+        return this.principal;
+    }
+
 }
